3ee2d0adffffe03819338c757ec65900fb2bbba5
Juan Wu <juanwu8238@gmail.com>
2025-05-02 17:40:21 -0400

    Definir main con su validacion

--- Asignacion_Modulo1.py
+++ Asignacion_Modulo1.py
@@ -10,6 +10,33 @@ class Tarea:
         self.id = id_tarea
         self.programadores_requeridos = programadores_requeridos
 
-# Pruba sencillo:
-pedro = Programador(12,2)
-print(pedro.id)
+# Aqui empezamos definir el main y con su validacion
+def main():
+    while True:
+        print(" Módulo de Asignación de Programadores a Tareas ")
+        print()
+        print("1. Ingresar datos manualmente")
+        print("2. Cargar datos desde archivo TXT")
+        print("3. Salir de programa")
+        
+        opcion = input("Seleccione una opción (1, 2 o 3): ")
+        
+        if opcion not in ['1', '2', '3']:
+            print()
+            print("¡Opción inválida! Por favor ingrese 1, 2 o 3.")
+            print()
+            continue
+        
+        if opcion == '1':
+            print("Opcion 1")
+            break
+        elif opcion == '2':
+            print("Opcion 2")
+            break
+        else:
+            print("Saliendo el programa......")
+            print("¡Hasta Luego!")
+            break
+
+# Una vez ha definido el main, llamamos para ejecutarlo
+main()
